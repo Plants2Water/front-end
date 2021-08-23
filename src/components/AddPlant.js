49e632218aedae3458f2c28203390996f1d5d73a
Erica Leonard <elynn1979@live.com>
2021-08-23 19:58:07 -0400

    work in progress

---
+++ src/components/AddPlant.js
@@ -0,0 +1,88 @@
+import React, {useState} from 'react'
+import {useHistory } from 'react-router-dom'
+import { Label } from 'reactstrap'
+
+const initialPlant = {
+    id: '',
+    nickname:'',
+    h20_frequency:'',
+    image: ''
+}
+
+const AddPlant = (props) => {
+    const [plant, setPlant] = useState(initialPlant)
+    const history = useHistory()
+    
+
+    const handleChange = event => {
+        setPlant({
+            ...plant,
+            [event.target.name]: event.target.value
+        })
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        props.AddPlant(plant)
+            }
+
+    return (
+        <div className = 'addPlantForm' id = 'addPlantForm'>
+            <form>
+                <h1>About Your Plant</h1>
+                <h2>
+
+            {/* PLANT NAME(NICKNAME) */}
+                <label >
+                    Plant Name:
+                    <input
+                        name = 'plantName'
+                        id = 'plantName'
+                        type = 'text'
+                        placeholder = 'Please name this plant'
+                        onChange = {handleChange}
+                    />
+                </label>
+
+            {/* PLANT SPECIES */}
+                <label>
+                    Species:
+                    <input
+                        name = 'species'
+                        id = 'species'
+                        type = 'text'
+                        placeholder = 'Plant Species'
+                        onChange = {handleSubmit}
+                    />
+                </label>
+
+            {/* H20 FREQUENCY */}
+            <label>
+                Water Frequency:
+                <input
+                    name = 'h20Frequency'
+                    id = 'h20Frequency'
+                    type = 'text'
+                    placeholder = 'How often to water'
+                    onChange = {handleSubmit}
+                />
+            </label>
+
+            {/* Optional Image */}
+            <Label>
+                <input 
+                    name = 'plantImage'
+                    id = 'plantImage'
+                    type = 'text'
+                    placeholder = 'Optional: URL to plant image'
+                    onChange = {handleSubmit}
+                />
+            </Label>
+                </h2>
+            </form>
+        </div>
+        
+    )
+}
+
+export default AddPlant
